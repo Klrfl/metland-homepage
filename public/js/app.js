b3726a500cac6b6e959d91468c7d26662782be92
Efraim Munthe <efrayanglain@gmail.com>
2023-04-28 11:41:11 +0700

    add submenu functionality

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,27 @@ const openNav = document.getElementById("open-nav");
 const closeNav = document.getElementById("close-nav");
 const mainNav = document.getElementById("main-nav");
 
-mainNav.addEventListener("click", () => {
+closeNav.addEventListener("click", () => {
   mainNav.classList.remove("active");
 });
 
 openNav.addEventListener("click", () => {
   mainNav.classList.toggle("active");
+});
+
+const navButtons = document.querySelectorAll("#main-nav > ul li button");
+const navSubMenus = document.querySelectorAll(".main-nav__sub-menu");
+
+navButtons.forEach((navButton) => {
+  const adjacentNavSubMenu = navButton.parentElement.children[1];
+
+  navButton.addEventListener("click", () => {
+    adjacentNavSubMenu.classList.toggle("active");
+
+    navSubMenus.forEach((navSubMenu) => {
+      if (navSubMenu !== adjacentNavSubMenu) {
+        navSubMenu.classList.remove("active");
+      }
+    });
+  });
 });
